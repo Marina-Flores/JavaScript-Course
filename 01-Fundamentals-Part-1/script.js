15c7d493ff589bb57c747c536b790f99f91943b9
Marina Flores <mlbtf@discente.ifpe.edu.br>
2023-02-05 10:07:33 -0300

    first commit

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,21 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10); 
+
+var firstName = 'Marina';
+console.log(firstName);
+
+/* 
+    LECTURE: Values and Variables
+
+    1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population in millions);
+    2. Log their values to the console
+
+    let country = 'Brazil',
+    let continent = 'South America';
+    let population = 214,3
+
+    console.log(country);
+    console.log(continent);
+    console.log(population);
+*/
+
